35205817d56014d02c31333adf17e671be93b4f5
paulorudiger <paulo.e.rudiger@gmail.com>
2024-06-06 20:55:33 -0300

    Alteracao BD

--- db/init.sql
+++ db/init.sql
@@ -1,55 +1,58 @@
-CREATE TABLE aux (
-    id int primary key not null,
-    teste varchar(30) not null
+-- Criação da Tabela: usuario
+CREATE TABLE usuario (
+    idusuario SERIAL PRIMARY KEY,
+    usuario VARCHAR(100) NOT NULL,
+    senha VARCHAR(50) NOT NULL,
+    nome VARCHAR(80) NOT NULL,
+    email VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE aluno (
-    idaluno SERIAL PRIMARY KEY,
-    matricula VARCHAR(50) NOT NULL UNIQUE,
-    dataNascimento DATE NOT NULL
-);
-
+-- Criação da Tabela: turma
 CREATE TABLE turma (
     idturma SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     turno VARCHAR(50) NOT NULL
 );
 
+-- Criação da Tabela: aluno
+CREATE TABLE aluno (
+    idaluno SERIAL PRIMARY KEY,
+    idusuario INTEGER,
+    matricula VARCHAR(50) NOT NULL UNIQUE,
+    dataNascimento DATE NOT NULL,
+    FOREIGN KEY (idusuario) REFERENCES usuario(idusuario)
+);
+
+-- Criação da Tabela: professor
 CREATE TABLE professor (
     idprofessor SERIAL PRIMARY KEY,
+    idusuario INTEGER,
     matricula VARCHAR(50) NOT NULL UNIQUE,
-    cargo VARCHAR(30) NOT NULL
+    cargo VARCHAR(30) NOT NULL,
+    FOREIGN KEY (idusuario) REFERENCES usuario(idusuario)
 );
 
-CREATE TABLE usuario (
-    idusuario SERIAL PRIMARY KEY,
-    idaluno INTEGER,
-    idprofessor INTEGER,
-    usuario VARCHAR(100) NOT NULL,
-    senha VARCHAR(50) NOT NULL,
-    nome VARCHAR(80) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    FOREIGN KEY (idaluno) REFERENCES aluno(idaluno),
-    FOREIGN KEY (idprofessor) REFERENCES professor(idprofessor)
-);
-
+-- Criação da Tabela: autor
 CREATE TABLE autor (
     idautor SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     nacionalidade VARCHAR(50) NOT NULL
 );
 
+-- Criação da Tabela: editora
 CREATE TABLE editora (
     ideditora SERIAL PRIMARY KEY,
     nomeEditora VARCHAR(100) NOT NULL,
     endereco VARCHAR(150) NOT NULL
 );
 
+-- Criação da Tabela: genero
 CREATE TABLE genero (
     idgenero SERIAL PRIMARY KEY,
     nomeGenero VARCHAR(100) NOT NULL
 );
 
+-- Criação da Tabela: livro
 CREATE TABLE livro (
     idlivro SERIAL PRIMARY KEY,
     idautor INTEGER NOT NULL,
@@ -63,6 +66,7 @@ CREATE TABLE livro (
     FOREIGN KEY (idgenero) REFERENCES genero(idgenero)
 );
 
+-- Criação da Tabela: aluguel
 CREATE TABLE aluguel (
     idaluguel SERIAL PRIMARY KEY,
     idusuario INTEGER NOT NULL,
@@ -74,6 +78,7 @@ CREATE TABLE aluguel (
     FOREIGN KEY (idlivro) REFERENCES livro(idlivro)
 );
 
+-- Criação da Tabela: autor_editora
 CREATE TABLE autor_editora (
     idautor INTEGER NOT NULL,
     ideditora INTEGER NOT NULL,
@@ -82,6 +87,7 @@ CREATE TABLE autor_editora (
     FOREIGN KEY (ideditora) REFERENCES editora(ideditora)
 );
 
+-- Criação da Tabela: aluno_turma
 CREATE TABLE aluno_turma (
     idaluno INTEGER NOT NULL,
     idturma INTEGER NOT NULL,
@@ -92,3 +98,4 @@ CREATE TABLE aluno_turma (
     FOREIGN KEY (idturma) REFERENCES turma(idturma),
     FOREIGN KEY (idprofessor) REFERENCES professor(idprofessor)
 );
+
